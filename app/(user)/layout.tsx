75404bc20396d3a1867de9d3490c17f6945d9173
Aayush Yadav <yadavaayush002@gmail.com>
2023-02-12 19:42:33 +0530

    Added footer and made changes

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,5 +1,6 @@
 
 import Banner from "../../components/Banner";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import "../../styles/globals.css";
 import Head from "../head";
@@ -14,16 +15,24 @@ export default function RootLayout({
   return (
     <html>
 
-      <body className="max-w-7xl mx-auto">
+      <body className= "mx-auto">
+        <div className="max-w-7xl mx-auto">
 
         {/* HEADER */}
         <Header />
         {/* {Header} */}
-        <Banner/>
+        <Banner />
 
         {/* BANNER */}
         {children}
+        </div>
+        
+        <Footer/>
+        
       </body>
+
+      
+
     </html>
   )
 }
